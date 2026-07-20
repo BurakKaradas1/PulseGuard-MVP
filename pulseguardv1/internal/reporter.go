43ca069e7235c5b18d195c6050f1354b5c5a7dc0
BurakKaradas1 <burakkaradas71@gmail.com>
2026-07-20 14:18:05 +0300

    feat: implement HMAC-SHA256 signature for payload integrity

--- pulseguardv1/internal/reporter.go
+++ pulseguardv1/internal/reporter.go
@@ -2,12 +2,23 @@ package internal
 
 import (
 	"bytes"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
 	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
 )
 
+const secretKey = "super-secret-pulseguard-key"
+
+// Veriyi gizli anahtarla şifrelediğimiz yer
+func generateSignature(payload []byte, secret string) string {
+	h := hmac.New(sha256.New, []byte(secret))
+	h.Write(payload)
+	return hex.EncodeToString(h.Sum(nil))
+}
 func SendBatch(events []Event, url string) error {
 	if len(events) == 0 {
 		return nil
@@ -17,11 +28,18 @@ func SendBatch(events []Event, url string) error {
 	if err != nil {
 		return err
 	}
+	//Verinin dijital mührü
+	signature := generateSignature(data, secretKey)
+
+	//Özel HTTP başlıkları ekleyebilmek için yeni Request
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-PulseGuard-Signature", signature)
 
 	client := &http.Client{Timeout: 5 * time.Second}
+	resp, err := client.Do(req)
 
-	//Veriyi C2 ye post et
-	resp, err := client.Post(url, "application/json", bytes.NewBuffer(data))
 	if err != nil {
 		return err
 	}
