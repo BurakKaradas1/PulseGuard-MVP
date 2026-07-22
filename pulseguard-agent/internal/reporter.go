df0112f3d5070e7403faa267462c401f4dabf825
BurakKaradas1 <burakkaradas71@gmail.com>
2026-07-22 16:42:21 +0300

    implement automated agent registration and professional URL routing

--- pulseguard-agent/internal/reporter.go
+++ pulseguard-agent/internal/reporter.go
@@ -7,7 +7,10 @@ import (
 	"encoding/hex"
 	"encoding/json"
 	"fmt"
+	"net"
 	"net/http"
+	"os"
+	"runtime"
 	"time"
 )
 
@@ -19,7 +22,65 @@ func generateSignature(payload []byte, secret string) string {
 	h.Write(payload)
 	return hex.EncodeToString(h.Sum(nil))
 }
-func SendBatch(events []Event, url string) error {
+
+// Ajan kayıt paket yapısı
+type RegisterPayload struct {
+	ID       string `json:"id"`
+	Hostname string `json:"hostname"`
+	IP       string `json:"ip_address"`
+	OS       string `json:"os"`
+}
+
+func getLocalIP() string {
+	addrs, err := net.InterfaceAddrs()
+	if err != nil {
+		return "127.0.0.1"
+	}
+	for _, address := range addrs {
+		if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+			if ipnet.IP.To4() != nil {
+				return ipnet.IP.String()
+			}
+		}
+	}
+	return "127.0.0.1"
+}
+
+// Ajanın Collector'e kendini kaydettirmesi
+func RegisterAgent(collectorURL string) error {
+	hostname, err := os.Hostname()
+	if err != nil {
+		hostname = "unknown-host"
+	}
+
+	payload := RegisterPayload{
+		ID:       hostname + "-agent",
+		Hostname: hostname,
+		IP:       getLocalIP(),
+		OS:       runtime.GOOS,
+	}
+
+	jsonData, err := json.Marshal(payload)
+	if err != nil {
+		return fmt.Errorf("JSON oluşturma hatası %v", err)
+	}
+
+	registerEndpoint := fmt.Sprintf("%s/api/v1/agent/register", collectorURL)
+	resp, err := http.Post(registerEndpoint, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("Collector'e ulaşılamadı: %v", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("Kayıt reddedildi, Durum Kodu: %d", resp.StatusCode)
+	}
+
+	fmt.Printf("[+] Ajan başarıyla Collector'e kaydedildi! (Host: %s, IP: %s)\n", payload.Hostname, payload.IP)
+	return nil
+}
+
+func SendBatch(events []Event, fullURL string) error {
 	if len(events) == 0 {
 		return nil
 	}
@@ -28,11 +89,15 @@ func SendBatch(events []Event, url string) error {
 	if err != nil {
 		return err
 	}
+
 	//Verinin dijital mührü
 	signature := generateSignature(data, secretKey)
 
 	//Özel HTTP başlıkları ekleyebilmek için yeni Request
-	req, err := http.NewRequest("POST", url, bytes.NewBuffer(data))
+	req, err := http.NewRequest("POST", fullURL, bytes.NewBuffer(data))
+	if err != nil {
+		return err
+	}
 
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("X-PulseGuard-Signature", signature)
